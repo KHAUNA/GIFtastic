a6ac3d2463ce512a26f09c646046f61d988fa0c5
KHAUNA <jtwinturbo@gmail.com>
2018-12-28 09:54:18 -0500

    upload

---
+++ javascript/gifyApp.js
@@ -0,0 +1,71 @@
+// api url
+// http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=YOUR_API_KEY
+// api key : ClQtp7UovcQoe3gclsYj7juncwa6X7CK
+
+//starting array
+var topics = ["bender","roberto","exploding eddie","bad santa",
+"professor farnsworth","zoidberg","leela","spongebob","patrick star",
+"squidward","homer simpson","bart simpson","peter griffin","ned flanders"]
+var response = "";
+
+$(document).ready(function() {
+
+//turns starting array into buttons and puts them on the page
+for (i=0; i < topics.length; i++) {
+    var newButton = $("<button>")
+    // newButton.attr("id", "btnss")
+    newButton.addClass("btns").text(topics[i])
+    $("#gif-buttons").append(newButton)
+}
+//grabs value entered by user and appends new button
+// CURRENT ERROR: new buttons do not seem to receive class .btn 
+// when called in following on click function...however CSS they receive same styling...wtf
+$(".btn").on("click", function() {
+    var addBtn = $(".form-control").val();
+    var addedButton = $("<button>")
+    topics.push(addBtn)
+    addedButton.addClass("btns").text(addBtn)
+    $("#gif-buttons").append(addedButton)
+});
+
+$(".btns").on("click", function() {
+    var searchBtn = $(this).text()
+    var key = "ClQtp7UovcQoe3gclsYj7juncwa6X7CK"
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchBtn + "&api_key=" + key
+    // - add line to remove old contents of div when a new button is pressed
+    // $("gif-div-container").remove();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        for (i=0; i<10; i++) {
+            var gifImg = $("<img>");
+            var gifDiv = $("<div>");
+            var gifRating = $("<h6>")
+            gifImg.attr("src", response.data[i].images.fixed_height_still.url);
+            gifImg.attr("data-stop", response.data[i].images.fixed_height_still.url);
+            gifImg.attr("data-play", response.data[i].images.fixed_height.url);
+            gifImg.attr("data-state", "stop");
+            gifRating.text("GIF Rating: " + response.data[i].rating);
+            gifDiv.append(gifImg);
+            gifDiv.append(gifRating);
+            $("#gif-div-container").append(gifDiv);
+            
+        }
+        $("img").on("click", function(){
+            var state = $(this).attr("data-state")
+            if (state === "stop") {
+                // gifImg.attr("src", $(this).attr("data-play")) - why doesnt this work as well as using $(this)
+                $(this).attr("src", $(this).attr("data-play"))
+                $(this).attr("data-state", "play")
+                console.log($(this).attr("data-state"))
+            } else {
+                $(this).attr("src", $(this).attr("data-stop"))
+                $(this).attr("data-state", "stop")
+            }
+        });
+    });
+});
+
+
+});
