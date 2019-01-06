71bc29f9c7ad33f9eaee5fe4eb05a165c5c7bc6c
KHAUNA <jtwinturbo@gmail.com>
2019-01-05 20:10:14 -0500

    final

---
+++ assets/README.txt
@@ -0,0 +1,3 @@
+GIFTastic App
+
+This app can be used to load 10 gifs from your entered search paramaters.  The app will load with prepopulated options, but feel free to add your own for searching.
--- javascript/gifyApp.js
+++ javascript/gifyApp.js
@@ -24,48 +24,49 @@ $(".btn").on("click", function() {
     var addBtn = $(".form-control").val();
     var addedButton = $("<button>")
     topics.push(addBtn)
-    addedButton.addClass("btns").text(addBtn)
+    addedButton.addClass("btns")
+    addedButton.text(addBtn)
     $("#gif-buttons").append(addedButton)
 });
 
-$(".btns").on("click", function() {
-    var searchBtn = $(this).text()
-    var key = "ClQtp7UovcQoe3gclsYj7juncwa6X7CK"
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchBtn + "&api_key=" + key
-    // - add line to remove old contents of div when a new button is pressed
-    // $("gif-div-container").remove();
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        for (i=0; i<10; i++) {
-            var gifImg = $("<img>");
-            var gifDiv = $("<div>");
-            var gifRating = $("<h6>")
-            gifImg.attr("src", response.data[i].images.fixed_height_still.url);
-            gifImg.attr("data-stop", response.data[i].images.fixed_height_still.url);
-            gifImg.attr("data-play", response.data[i].images.fixed_height.url);
-            gifImg.attr("data-state", "stop");
-            gifRating.text("GIF Rating: " + response.data[i].rating);
-            gifDiv.append(gifImg);
-            gifDiv.append(gifRating);
-            $("#gif-div-container").append(gifDiv);
-            
-        }
-        $("img").on("click", function(){
-            var state = $(this).attr("data-state")
-            if (state === "stop") {
-                // gifImg.attr("src", $(this).attr("data-play")) - why doesnt this work as well as using $(this)
-                $(this).attr("src", $(this).attr("data-play"))
-                $(this).attr("data-state", "play")
-                console.log($(this).attr("data-state"))
-            } else {
-                $(this).attr("src", $(this).attr("data-stop"))
-                $(this).attr("data-state", "stop")
+$('#gif-buttons').on('click', '.btns', function() {
+    $(".btns").on("click", function() {
+        var searchBtn = $(this).text()
+        var key = "ClQtp7UovcQoe3gclsYj7juncwa6X7CK"
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchBtn + "&api_key=" + key
+        // - add line to remove old contents of div when a new button is pressed
+        // $("gif-div-container").remove();
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            for (i=0; i<12; i++) {
+                var gifImg = $("<img>");
+                var gifDiv = $("<div>");
+                var gifRating = $("<h6>")
+                gifImg.attr("src", response.data[i].images.fixed_height_still.url);
+                gifImg.attr("data-stop", response.data[i].images.fixed_height_still.url);
+                gifImg.attr("data-play", response.data[i].images.fixed_height.url);
+                gifImg.attr("data-state", "stop");
+                gifRating.text("GIF Rating: " + response.data[i].rating);
+                gifDiv.append(gifImg);
+                gifDiv.append(gifRating);
+                $("#gif-div-container").append(gifDiv);
             }
+            $("img").on("click", function(){
+                var state = $(this).attr("data-state")
+                if (state === "stop") {
+                    // gifImg.attr("src", $(this).attr("data-play")) - why doesnt this work as well as using $(this)
+                    $(this).attr("src", $(this).attr("data-play"))
+                    $(this).attr("data-state", "play")
+                    console.log($(this).attr("data-state"))
+                } else {
+                    $(this).attr("src", $(this).attr("data-stop"))
+                    $(this).attr("data-state", "stop")
+                }
+            });
         });
     });
 });
-
 
 });
